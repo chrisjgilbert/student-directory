9c274d6ef5546bf88ebdd269ebb0508472d9e3ad
Chris Gilbert <chrisgilbert4@icloud.com>
2018-10-17 10:34:42 +0100

    initial program working

--- student-directory/directory.rb
+++ student-directory/directory.rb
@@ -34,8 +34,8 @@ def print_header
 end
 
 def print_list(students)
-  students.each.with_index(1) do |student,index|
-    puts "#{index}.#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
