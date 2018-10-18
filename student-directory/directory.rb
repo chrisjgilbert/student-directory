b9ad8e449727cf984dbf8a895fa67c32b6286b90
Chris Gilbert <chrisgilbert4@icloud.com>
2018-10-18 15:17:59 +0100

    merged from interactive_directory.rb

--- student-directory/directory.rb
+++ student-directory/directory.rb
@@ -1,49 +1,86 @@
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Norman Bates", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Patrick Bateman", cohort: :november},
-  {name: "Tyler Durden", cohort: :november},
-  {name: "Freddy Kreuger", cohort: :november},
-  {name: "The Joker", cohort: :november},
-  {name: "Dr No", cohort: :november}
-]
+@students = [] # an empty array accessible to all methods
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def save_students
+  file = File.open("students.csv", "w")
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts(csv_line)
+  end
+  file.close
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort = line.chomp.split(',')
+    @students << {name: name, cohort: cohort.to_sym}
+  end
+  file.close
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save student list to students.csv"
+  puts "4. Load the student list from students.csv"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_student_list
+  print_footer
+end
 
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
-  # get first name
   name = gets.chomp
   while !name.empty? do
-    # add students to array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
-    # get another name from the user
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     name = gets.chomp
   end
-  # return students array
-  students
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      save_students
+    when "4"
+      load_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean. Please try again."
+  end
 end
 
 def print_header
-  puts "The Students of Villains Academy"
-  puts "===================="
+  puts "The students of my cohort at Makers Academy"
+  puts "-------------"
 end
 
-def print_list(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+def print_student_list
+  @students.each.with_index(1) do |student, index|
+    puts "#{index}: #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-print "Overall, we have #{students.count} number of great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
-students = input_students
-print_header
-print_list(students)
-print_footer(students)
+interactive_menu
