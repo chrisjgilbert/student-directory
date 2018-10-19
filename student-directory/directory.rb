bc94910baf30560e54d612c0de8918d1447457a2
Chris Gilbert <chrisgilbert4@icloud.com>
2018-10-19 18:00:46 +0100

    load_students refactored

--- student-directory/directory.rb
+++ student-directory/directory.rb
@@ -52,7 +52,7 @@ def load_students
     puts "Loaded #{@students.count} students from #{filename}"
     file.close
   else
-    puts "Sorry, #{filename} doesn't exist!"
+    puts "Sorry, '#{filename}' doesn't exist!"
   end
 end
 
